32d168104b7e53eaba933e849af662490ed18ec7
Jorge Tejeda Bello <jtejeda@jorges-mbp.lan>
2024-06-02 21:22:48 -0400

    configured bufferline tabs

--- lua/may/plugins/bufferline.lua
+++ lua/may/plugins/bufferline.lua
@@ -2,10 +2,15 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  keys = {
+    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer" },
+  },
   opts = {
     options = {
       mode = "tabs",
-      separator_style = "slant",
+      show_buffer_close_icons = false,
+      show_close_icon = false,
     },
   },
 }
